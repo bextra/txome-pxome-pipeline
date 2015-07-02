68ddbeddd9372b85a3dea38db6b0bdd075720f36
kbeck527 <kristenbeck527@gmail.com>
2015-07-02 11:23:00 -0700

    updated for multiple bam files for trinity run om isner

--- run_trinity_genomeguided_isner.sh
+++ run_trinity_genomeguided_isner.sh
@@ -4,19 +4,22 @@
 # the reads were first aligned to rheMac2
 # Trinity will partition according to locus, followed by de novo transcriptome assmebly at each locus
 
+
+
 #Read alignments must be supplied as coordinate-sorted bam file.
 cd /Chanlab/Scratch/kristen/Macaque_transcriptome/BAMs
 
-#SAMPLE_LIST="$(ls)"
-
-SAMPLE_LIST="monkey_2_1_index18.bam"
+SAMPLE_LIST="$(ls *.bam)"
 
 for FILE in $SAMPLE_LIST
 do
-	samtools sort $FILE sorted_$FILE
+	samtools sort -f $FILE sorted_$FILE
 done
 
+export JAVA_HOME=/Chanlab/Packages/Java/jdk1.7.0_80/bin/java
+export PATH="/Chanlab/Packages/Java/jdk1.7.0_80/bin/:$PATH"
+
 for FILE in $SAMPLE_LIST
 do
-	/Chanlab/Packages/Trinity/trinityrnaseq-2.0.6/Trinity --genome_guided_bam sorted_$FILE --genome_guided_max_intron 10000 -seqType fq --max_memory 20G --CPU 20 --output ./trinity_gg_$FILE --full_cleanup
+	time /Chanlab/Packages/Trinity/trinityrnaseq-2.0.6/Trinity --genome_guided_bam /Chanlab/Scratch/kristen/Macaque_transcriptome/BAMs/sorted_$FILE --genome_guided_max_intron 10000 -seqType fq --max_memory 70G --CPU 20 --output ./trinity_gg_$FILE --full_cleanup > stdout_run_$FILE
 done
