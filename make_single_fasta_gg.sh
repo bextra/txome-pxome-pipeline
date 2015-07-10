e69a98bf839803efb19c9d7d2b05a13168b7b38e
kbeck527 <kristenbeck527@gmail.com>
2015-07-10 14:09:45 -0700

    added loop to deal with very large directory for each sixpack translation of the transcriptome

--- make_single_fasta_gg.sh
+++ make_single_fasta_gg.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # After Sixpack has run, it creates seperate files for each single sequence
 # These need to be combined into one single fasta file of all possible ORFs
-echo "starting cat"
-cat ./output_Sixpack_gg/*.fasta > denovo_Sixpack_ORFs_m2_1_i18_gg.fasta 
-echo "completed cat"
+
+echo "starting fasta file concatenation"
+
+# files must be listed for the full folder
+# subsetting for fasta only will overload "ls"
+ls ./output_Sixpack_gg/ > list_sixpack_outputs.txt
+grep "fasta" list_sixpack_outputs.txt > list_sixpack_outputs2.txt
+echo "files listed"
+
+echo "" > genome_guided_merged.fasta # prevent adding a non-empty file
+
+while read file; do
+	cat ./output_Sixpack_gg/$file >> genome_guided_merged.fasta
+done < list_sixpack_outputs2.txt
+
+# clean up
+rm list_sixpack_outputs2.txt
+rm list_sixpack_outputs.txt
+
+echo "completed concatentation"
