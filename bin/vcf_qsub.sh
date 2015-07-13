1325820f77238f18e766257bb5aad38f51eedb9a
kbeck527 <kristenbeck527@gmail.com>
2015-07-13 12:11:41 -0700

    updated script to allow bam files from different location

--- bin/vcf_qsub.sh
+++ bin/vcf_qsub.sh
@@ -9,11 +9,12 @@ export PATH
 
 #make writing file names easier
 FILEROOT=/share/milklab/proteomics/VariantCalling
+BAMPATH=/share/milklab/proteomics/BAM_files
 
 # generate VCF file
-# for macaque
+# for macaque:
 # samtools mpileup -DVuf $FILEROOT/ReferenceData/rheMac2.fa $FILEROOT/*.bam | bcftools call -vm > $FILEROOT/updated_monkey_pxtx_paired.vcf
 
-# for human
-samtools mpileup -DVuf $FILEROOT/ReferenceData/human_ensembl.GRCh37.fa $FILEROOT/human*.bam | bcftools call -vm > $FILEROOT/human_pxtx_paired.vcf
+# for human:
+samtools mpileup -DVuf $FILEROOT/ReferenceData/human_ensembl.GRCh37.fa $BAMPATH/human*.bam | bcftools call -vm > $FILEROOT/human_pxtx_paired_all_tech_reps.vcf
 
