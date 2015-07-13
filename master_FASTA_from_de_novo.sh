5c77a9c7fbb9790614c6a75c0599b48c929450d9
kbeck527 <kristenbeck527@gmail.com>
2015-07-13 15:35:25 -0700

    transferred Danielle 's readme info to master scripts

---
+++ master_FASTA_from_de_novo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# To create de novo transcriptomes, the sequence of scripts used:
+#
+# 1) run_trinity.csh
+# 2) make_multiple_fasta.csh
+# 3) run_sixpack.csh
+# 4) make_single_fasta.csh
---
+++ master_FASTA_from_genome_guided.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# then to repeat above for a genome-guided transcriptome:
+# 1) run_trinity_genomeguided.csh
+# 2) make_multiple_fasta_gg.csh
+# 3) run_sixpack_gg.csh
+# 4) make_single_fasta_gg.csh
