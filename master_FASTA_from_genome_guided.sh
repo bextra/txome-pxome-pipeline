3758cf85871efef96660643f1e4a9e5fc975fc67
kbeck527 <kristenbeck527@gmail.com>
2015-07-14 10:21:50 -0700

    updated first step to getting genome guided fasta

--- master_FASTA_from_genome_guided.sh
+++ master_FASTA_from_genome_guided.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# then to repeat above for a genome-guided transcriptome:
-# 1) run_trinity_genomeguided.csh
+# 1) Make a genome guided assembly
+run_trinity_genomeguided.csh
+
 # 2) make_multiple_fasta_gg.csh
 # 3) run_sixpack_gg.csh
 # 4) make_single_fasta_gg.csh
